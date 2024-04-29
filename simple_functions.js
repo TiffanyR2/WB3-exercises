00b19bc503d8f0bdad99f07290ec6030cdd5ff9c
Tiffanyr1 <166432598+Tiffanyr1@users.noreply.github.com>
2024-04-29 10:26:05 -0400

    functions creation

---
+++ simple_functions.js
@@ -0,0 +1,25 @@
+function favoriteThings(){
+    console.log("Hello My Name Is Tiffany,")
+
+    console.log("My Favorite Movie is B.A.P.S")
+
+    console.log("My Favorite Band Is NSYNC.")
+
+}
+
+favoriteThings()
+
+function whyImHere(){
+
+    console.log("Im Joining the Learn to Code Program to Become a Software Developer.")
+}
+
+whyImHere()
+function favoritePlace(){
+
+console.log("One of My Favorite Places To Visit Is Nevada Because Of All Of The Nice Sceneries, and of Course Visiting Vegas While There.")
+
+}
+
+favoritePlace()
+
