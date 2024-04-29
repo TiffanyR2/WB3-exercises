418ba5c6d88564030676b8fe0287ea9cace94f83
Tiffanyr1 <166432598+Tiffanyr1@users.noreply.github.com>
2024-04-29 14:49:43 -0400

    update

--- more_functions.js
+++ more_functions.js
@@ -9,21 +9,21 @@ displayMailingLabel("Tiffany", "1508 9th St","Dade City", "FL", "33765")
 function addNumbers(num1, num2){
     console.log(num1, "+", num2, "=", num1 + num2)
 }
-console.log("")
+    console.log("")
 
     addNumbers(50,50)   
-console.log ("")
+    console.log ("")
 
 function displayReceipt(totalDue, amountPaid){
 
-console.log("totaldue", totalDue)
-console.log("amountPaid", amountPaid)
-console.log("changeDue", amountPaid - totalDue);
+    console.log("totaldue", totalDue)
+    console.log("amountPaid", amountPaid)
+    console.log("changeDue", amountPaid - totalDue);
 }
 displayReceipt(25,100);
-console.log("")
+    console.log("")
 
 displayReceipt(25,25);
-console.log("")
+    console.log("")
 
 displayReceipt(25,18);
